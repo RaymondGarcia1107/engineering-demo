0719da4a0441a05bd5df3283b24bc8ab1a8f5952
Raymond Garcia <112764506+RaymondGarcia1107@users.noreply.github.com>
2025-04-29 11:11:23 -0400

    made a daily upload file

---
+++ app/dailydataload.py
@@ -0,0 +1,52 @@
+from faker import Faker
+import psycopg2
+from psycopg2.extras import execute_values
+import random
+
+# Initializing a faker object
+fake = Faker()
+
+# Connecting to the database and setting up a cursor
+conn = psycopg2.connect(
+    host = "18.189.178.29",
+    database = "random_data",
+    user = "admin",
+    password = "admin",
+    port = 5432
+)
+cur = conn.cursor()
+print("DB Connected")
+
+# Generating fake user data
+user_data = [(fake.name(), fake.email(), fake.address()) for _ in range(5)]
+
+# Inserting into the user table and committing
+execute_values(cur, "INSERT INTO users (name, email, address) VALUES %s", user_data)
+conn.commit()
+print("User data inserted")
+
+# Gathering the id's to generate transactions on
+cur.execute("select id from users;")
+user_ids = [row[0] for row in cur.fetchall()]
+print("ID's gathered")
+
+# Generating random data
+transaction_types = ['purchase', 'refund', 'transfer', 'payment']
+data = [
+    (
+        random.choice(user_ids),
+        round(random.uniform(10.0, 10000.0), 2),
+        random.choice(transaction_types)
+    )
+    for _ in range(50)
+]
+
+# Inserting the values into the transactions table and committing
+execute_values(cur, "INSERT INTO transactions (user_id, amount, transaction_type) VALUES %s", data)
+conn.commit()
+print("Transaction data inserted")
+
+# Closing connections
+cur.close()
+conn.close()
+print("Connections closed. All clear")
